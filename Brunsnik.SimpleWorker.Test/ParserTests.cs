c1c5647783ebf4ef5cc5d4a1f80105c4b291183f
Peter Berry <pwcberry@gmail.com>
2026-06-26 16:41:22 +1000

    Add unit tests

---
+++ Brunsnik.SimpleWorker.Test/ParserTests.cs
@@ -0,0 +1,26 @@
+﻿using Brunsnik.SimpleWorker.Conversion;
+
+namespace Brunsnik.SimpleWorker.Test;
+
+public class ParserTests
+{
+    [Fact]
+    public void Parse_CreatesSetDocumentWithPointElements()
+    {
+        var parser = new Parser();
+        using var reader = new StringReader("1 2\n3 4");
+
+        var document = parser.Parse(reader);
+
+        Assert.NotNull(document);
+        Assert.NotNull(document!.Root);
+        Assert.Equal("Set", document.Root!.Name.LocalName);
+
+        var points = document.Root.Elements("Point").ToList();
+        Assert.Equal(2, points.Count);
+        Assert.Equal("1", points[0].Attribute("X")?.Value);
+        Assert.Equal("2", points[0].Attribute("Y")?.Value);
+        Assert.Equal("3", points[1].Attribute("X")?.Value);
+        Assert.Equal("4", points[1].Attribute("Y")?.Value);
+    }
+}
---
+++ Brunsnik.SimpleWorker.Test/TokenizerTests.cs
@@ -0,0 +1,32 @@
+﻿using Brunsnik.SimpleWorker.Conversion;
+
+namespace Brunsnik.SimpleWorker.Test;
+
+public class TokenizerAndParserTests
+{
+    [Fact]
+    public void Tokenize_ReturnsOnlyMatchingLines_AsTokens()
+    {
+        using var reader = new StringReader("10 20\nignored\n30 40");
+
+        var tokens = Tokenizer.Tokenize(reader).ToList();
+
+        Assert.Equal(2, tokens.Count);
+        Assert.Equal(10, tokens[0].Item1);
+        Assert.Equal(20, tokens[0].Item2);
+        Assert.Equal(30, tokens[1].Item1);
+        Assert.Equal(40, tokens[1].Item2);
+    }
+
+    [Fact]
+    public void Tokenize_IgnoresNonMatchingContent()
+    {
+        using var reader = new StringReader("no tokens here\nstill nothing\n1 2");
+
+        var tokens = Tokenizer.Tokenize(reader).ToList();
+
+        Assert.Single(tokens);
+        Assert.Equal(1, tokens[0].Item1);
+        Assert.Equal(2, tokens[0].Item2);
+    }
+}
